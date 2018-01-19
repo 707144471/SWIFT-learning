d44526adbf967e3e68ce8e06c8b673a447bc851c
hanshaolin <707144471@qq.com>
2018-01-19 22:03:54 +0800

    给图片添加手势选择本地相框图片

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,16 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITextFieldDelegate {
+class ViewController: UIViewController,UITextFieldDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     //MARK: Properties-命名并且链接xib
     
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         nameTextField.delegate=self
+        photoImageView.isUserInteractionEnabled=true;
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,10 +38,46 @@ class ViewController: UIViewController,UITextFieldDelegate {
         mealNameLabel.text=textField.text
         print("我第一个执行")
     }
+    //MARK: UIImagePickerControllerDelegate 图片选择器
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.这段代码激发了图像采集器控制器的解雇
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+
+
     
 //MARK: Actions-按钮
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
+
     }
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        // Hide the keyboard.此代码可确保如果用户在文本字段中键入图像视图时，键盘将被正确解除。
+        nameTextField.resignFirstResponder()
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        // Only allow photos to be picked, not taken.这一行代码设置图像选择器控制器的来源，或者获取图像的地方。该.photoLibrary选项使用模拟器的相机胶卷
+        imagePickerController.sourceType = .photoLibrary
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        /*
+         present(_:animated:completion:)是一个被调用的方法ViewController。虽然没有明确写出，但是这个方法是在一个隐含的self对象上执行的。该方法要求ViewController呈现由定义的视图控制器imagePickerController。传递true给animated参数可激活图像采集器控制器的显示。该completion参数是指完成处理程序，即在该方法完成后执行的一段代码。因为你不需要做任何事情，所以你指出你不需要通过传入nil来执行完成处理程序。
+         */
+        present(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    
 }
 
